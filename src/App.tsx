c29e0c2ab635d5d4b132bcf90e55d79d0a40a93f
kim_minju <kim_minju@Minjus-MacBook-Air.local>
2023-07-25 22:28:05 +0100

    hover cursor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,83 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from "styled-components";
+import inspiration from "/inspiration.jpeg";
+
+const StyledCursor = styled.div<{ value: string }>`
+  height: 150px;
+  width: 150px;
+  text-align: center;
+  background-color: #303030;
+  color: #fff;
+  border-radius: 15px;
+  cursor: ${(props) => props.value};
+`;
+
+const StyledContainer = styled.div<{}>`
+  display: flex;
+  flex-flow: wrap;
+  gap: 10px;
+  justify-content: center;
+  @media (min-width: 700px) {
+    margin-top: 30px;
+    max-width: 1200px;
+  }
+  @media (max-width: 699px) {
+    margin-top: 10px;
+  }
+  margin: 0 auto;
+  img {
+    @media (min-width: 800px) {
+      max-width: 900px;
+    }
+    @media (max-width: 799px) {
+      max-width: 600px;
+    }
+  }
+`;
+
+const StyledTitle = styled.h1<{}>`
+  text-align: center;
+`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cursorList = [
+    "default",
+    "wait",
+    "progress",
+    "not-allowed",
+    "no-drop",
+    "copy",
+    "context-menu",
+    "help",
+    "move",
+    "all-scroll",
+    "cursor",
+    "pointer",
+    "grab",
+    "grabbing",
+    "cell",
+    "zoom-in",
+    "zoom-out",
+    "row-resize",
+    "col-resize",
+    "nwse-resize",
+    "alias",
+  ];
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <StyledTitle>:hover cursor:</StyledTitle>
+      <StyledContainer>
+        {cursorList.map((element, index) => (
+          <StyledCursor key={index} value={element}>
+            {element}
+          </StyledCursor>
+        ))}
+      </StyledContainer>
+      <StyledContainer>
+        <img src={inspiration} alt="inspiration jpeg" />
+      </StyledContainer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
